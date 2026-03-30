e7ac3e8db0b51866aed577f52e505a20838f679a
SAMANVITA DHARWADKAR <samanvitard@gmail.com>
2026-03-30 20:34:20 +0530

    Fix import statement for mobile/his_request_test.dart

--- integration_test/runner.dart
+++ integration_test/runner.dart
@@ -3,7 +3,7 @@ import 'desktop/env_manager_test.dart';
 import 'desktop/his_request_test.dart';
 import 'desktop/req_editor_test.dart';
 import 'mobile/env_manager_test.dart';
-import 'mobile/his_request_test.dart';
+import 'mobile/his_request_test.dart'; 
 import 'mobile/req_editor_test.dart';
 
 Future<void> main() async {
